3f427fc107ec918bd53e816037aeeaf10a7aee7c
Mark Lokhorst <mlokhorst@gmail.com>
2019-07-02 18:25:19 -0500

    imperial units

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -1,10 +1,10 @@
 //f8bf3ee912539bb48b67cc3dfc427099
 const weatherObject = new XMLHttpRequest();
 
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=55386,us&appid=f8bf3ee912539bb48b67cc3dfc427099",true)
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=55386,us&appid=f8bf3ee912539bb48b67cc3dfc427099&units=imperial",true)
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
-    
+
 }//end of onload
