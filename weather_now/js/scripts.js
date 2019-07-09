5361218b25becfd0f6c683945a928fc4df59b939
Mark Lokhorst <mlokhorst@gmail.com>
2019-07-09 15:56:17 -0500

    set path to icon

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -14,4 +14,5 @@ weatherObject.onload = function() {
     const iconcode =weatherInfo.weather[0].icon;
     const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
     console.log(icon_path);
+    document.getElementById('weather_icon').src = icon_path;
 }//end of onload
